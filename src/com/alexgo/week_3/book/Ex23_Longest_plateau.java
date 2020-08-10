7b7c890e4cfb47f02d0a42e10a3ae3fb55ebc2ad
Alex Golub <55350522+Alex-Golub@users.noreply.github.com>
2020-08-10 14:02:03 +0300

    Week 3 - Book - 1.4 Arrays - Creative Exercises

---
+++ src/com/alexgo/week_3/book/Ex23_Longest_plateau.java
@@ -0,0 +1,19 @@
+package com.alexgo.week_3.book;
+
+/**
+ * 1.4.23 Longest plateau.
+ * Given an array of integers, find the length and location
+ * of the longest contiguous sequence of equal values
+ * for which the values of the elements just before and just after this sequence are smaller.
+ *
+ * Created by ag on 26-Jul-20 8:37 PM
+ */
+class Ex23_Longest_plateau {
+  public static void main(String[] args) {
+
+  }
+}
+
+/*
+    1 2 2 3 2 3 3 4 4 4
+*/
